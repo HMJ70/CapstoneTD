7e5c3c6f409a2fdf313a49596a16d715e6091a87
HMJ70 <hans.melvern1233@gmail.com>
2025-10-30 01:47:37 +0700

    Remove Platform when place tower

--- Assets/Scripts/Platforms.cs
+++ Assets/Scripts/Platforms.cs
@@ -1,34 +1,49 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class Platforms : MonoBehaviour
 {
     public static event Action<Platforms> OnPlatformsClicked;
     [SerializeField] private LayerMask platformlayermask;
     public static bool Tpanelopen { get; set; } = false;
+
+    [SerializeField] private SpriteRenderer explicitSpriteRenderer;
+
     private void Update()
     {
         if (Tpanelopen || Time.timeScale == 0f)
-        {
             return;
-        }
+
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
             Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             RaycastHit2D raycasthit = Physics2D.Raycast(worldPoint, Vector2.zero, Mathf.Infinity, platformlayermask);
-            if(raycasthit.collider != null)
+            if (raycasthit.collider != null)
             {
                 Platforms platforms = raycasthit.collider.GetComponent<Platforms>();
-                if(platforms != null)
-                {
+                if (platforms != null)
                     OnPlatformsClicked?.Invoke(platforms);
-                }
             }
         }
     }
 
     public void PlaceTower(TowerDatas data)
     {
-        Instantiate(data.prefab,transform.position,Quaternion.identity,transform);
+        SpriteRenderer[] platformRenderers = GetComponentsInChildren<SpriteRenderer>(true); 
+        if (explicitSpriteRenderer != null)
+        {
+            explicitSpriteRenderer.enabled = false;
+        }
+        else
+        {
+           
+            foreach (var sr in platformRenderers)
+            {
+               
+                sr.enabled = false;
+            }
+        }
+        GameObject towerGO = Instantiate(data.prefab, transform.position, Quaternion.identity, transform);
     }
 }
